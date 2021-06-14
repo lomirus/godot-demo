0308ae1c637aac6da6a266ce3e4b3f7e8b76edea
Rhys <lomirus@qq.com>
2021-06-14 22:52:16 +0800

    feat: add sekiro(Player)

---
+++ scripts/Player.gd
@@ -0,0 +1,35 @@
+extends KinematicBody2D
+
+var arms
+var legs
+
+func _ready():
+	arms = self.get_node("Arms")
+	legs = self.get_node("Legs")
+
+
+func _input(event):
+	if event.is_action_pressed("defense"):
+		arms.animation = "defense"
+	if event.is_action_released("defense"):
+		arms.animation = "normal"
+
+func _physics_process(_delta):
+	if Input.is_action_pressed("ui_left") || Input.is_action_pressed("ui_right"):
+		legs.animation = 'run'
+		if Input.is_action_pressed("ui_left"):
+			self.position.x -= 3
+			self.scale = Vector2(-1, 1)
+		if Input.is_action_pressed("ui_right"):
+			self.position.x += 3
+			self.scale = Vector2(1, 1)
+	if Input.is_action_just_released("ui_left") || Input.is_action_just_released("ui_right"):
+		legs.animation = 'stand'
+	self.position.x = clamp(self.position.x, 2, get_viewport_rect().size.x - 2)
+	self.position.y = clamp(self.position.y, 2, get_viewport_rect().size.y - 2)
+
+
+func _process(_delta):
+	pass
+	
+
